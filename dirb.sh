c64fb761dba894bb367a066e2655d3848d3efa36
Kerkroups <40368700+Kerkroups@users.noreply.github.com>
2020-11-13 15:09:59 +0200

    Update dirb.sh

--- dirb.sh
+++ dirb.sh
@@ -3,7 +3,7 @@
 for i in $(cat $1)
 do
     var="${i}"
-    dirb https://$i $2 -X .pdf,.doc,.xls,.ppt,.odp,.ods,.docx,.xlsx,.pptx -S -R -N 404 -i > $var  &
+    dirb https://$i $2 -X .pdf,.doc,.xls,.ppt,.odp,.ods,.docx,.xlsx,.pptx,.py,.db,.log,.dll,.zip,.tar,.php,.php3,.cnf,.git -S -R -N 404 -i > $var  &
 done
 wait
 echo Finished
