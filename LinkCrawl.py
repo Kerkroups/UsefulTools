f41907c97775c2870ccf6d29737481bce7b361d0
root <root@localhost.localdomain>
2020-12-10 22:32:24 +0200

    Add help to LinkCrawl.py

--- LinkCrawl.py
+++ LinkCrawl.py
@@ -2,24 +2,42 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
-import sys
+import re, sys
+from optspec import getopt
 
-response = urlopen(str(sys.argv[1]))
-data = response.read()
-encoding = response.info().get_content_charset()
-html = data.decode(encoding)
+def main (argv):
 
-soup = BeautifulSoup(html, "html.parser")
-links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]
-r = re.compile("https:\/\/\w+")
-https_list = list(filter(r.match, links_with_text))
-print("HTTPS LINKS")
-for i in https_list:
-    print(i)
-print("INTERNAL LINKS")
-r = re.compile("\/\w+")
-nonHTTP_list = list(filter(r.match, links_with_text))
-for link in nonHTTP_list:
-    print(link)
+    sitename = ""
+    try:
+        opts, args = getopt.getopt(argv,"hu:",["url="])
+    except getopt.GetoptError:
+        print ('LinkCrawl.py -u <https://sitename>')
+        sys.exit(2)
 
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('LinkCrawl.py -u <https://sitename>')
+            sys.exit()
+        elif opt in ("-u", "--url"):
+            sitename = str(arg)
+
+    response = urlopen(sitename)
+    data = response.read()
+    encoding = response.info().get_content_charset()
+    html = data.decode(encoding)
+
+    soup = BeautifulSoup(html, "html.parser")
+    links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]
+    r = re.compile("https:\/\/\w+")
+    https_list = list(filter(r.match, links_with_text))
+    print("HTTPS LINKS")
+    for i in https_list:
+        print(i)
+    print("INTERNAL LINKS")
+    r = re.compile("\/\w+")
+    nonHTTP_list = list(filter(r.match, links_with_text))
+    for link in nonHTTP_list:
+        print(link)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
