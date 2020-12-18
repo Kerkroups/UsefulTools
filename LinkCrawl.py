ea9a48ef3513fa77295424fe076b386306e826fd
root <root@localhost.localdomain>
2020-12-18 19:34:20 +0200

    Change script

--- LinkCrawl.py
+++ LinkCrawl.py
@@ -5,39 +5,21 @@ from bs4 import BeautifulSoup
 import re, sys
 from optspec import getopt
 
-def main (argv):
+sitename = sys.argv[1]
+response = urlopen(sitename)
+data = response.read()
+encoding = response.info().get_content_charset()
+html = data.decode(encoding)
 
-    sitename = ""
-    try:
-        opts, args = getopt.getopt(argv,"hu:",["url="])
-    except getopt.GetoptError:
-        print ('LinkCrawl.py -u <https://sitename>')
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print ('LinkCrawl.py -u <https://sitename>')
-            sys.exit()
-        elif opt in ("-u", "--url"):
-            sitename = str(arg)
-
-    response = urlopen(sitename)
-    data = response.read()
-    encoding = response.info().get_content_charset()
-    html = data.decode(encoding)
-
-    soup = BeautifulSoup(html, "html.parser")
-    links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]
-    r = re.compile("https:\/\/\w+")
-    https_list = list(filter(r.match, links_with_text))
-    print("HTTPS LINKS")
-    for i in https_list:
-        print(i)
-    print("INTERNAL LINKS")
-    r = re.compile("\/\w+")
-    nonHTTP_list = list(filter(r.match, links_with_text))
-    for link in nonHTTP_list:
-        print(link)
-
-if __name__ == "__main__":
-   main(sys.argv[1:])
+soup = BeautifulSoup(html, "html.parser")
+links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]
+r = re.compile("https:\/\/\w+")
+https_list = list(filter(r.match, links_with_text))
+print("HTTPS LINKS")
+for i in https_list:
+    print(i)
+print("INTERNAL LINKS")
+r = re.compile("\/\w+")
+nonHTTP_list = list(filter(r.match, links_with_text))
+for link in nonHTTP_list:
+    print(link)
